5980578f82a7e1b47e0ccbeb25a78aba24928f2b
qiannvyouhun <2504189164@qq.com>
2019-11-07 13:53:44 +0800

    work02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {HashRouter as Router,Route} from 'react-router-dom';
 import Home from './container/Home';
 import Api from './container/Api';
 import About from './container/About';
